051df04a389a76e661092f0ee4a45007671fd321
Pramod Perera <pramodkperera@outlook.com>
2022-01-16 12:11:46 +0530

    UI and BackEnd Update

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,12 +5,94 @@ import WeekPage from '../pages/week.f7';
 var routes = [
   {
     path: '/',
-    component: HomePage,
+    async: function ({ router, to, resolve }) {
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      navigator.geolocation.getCurrentPosition((success) => {
+
+        let { latitude, longitude } = success.coords;
+
+        app.request.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely&units=metric&appid=0f8cc38152a549b42988792a53581370')
+
+          .then(function (res) {
+            console.log('Load was performed');
+
+            // Hide Preloader
+            app.preloader.hide();
+
+            // Resolve route to load page
+            resolve(
+              {
+                component: HomePage,
+              },
+              {
+                props: {
+                  weatherData: JSON.parse(res.data),
+                }
+              }
+            );
+            //
+          }).catch(function (err) {
+            console.log(err.xhr)
+            console.log(err.status)
+            console.log(err.message)
+          });
+      });
+    },
   },
   {
     path: '/week/',
-    component: WeekPage,
-  },
+    async: function ({ router, to, resolve }) {
+
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      navigator.geolocation.getCurrentPosition((success) => {
+
+        let { latitude, longitude } = success.coords;
+
+        app.request.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely&units=metric&appid=0f8cc38152a549b42988792a53581370')
+
+          .then(function (res) {
+            console.log('Load was performed');
+
+            // Hide Preloader
+            app.preloader.hide();
+
+            // Resolve route to load page
+            resolve(
+              {
+                component: WeekPage,
+              },
+              {
+                props: {
+                  weatherData: JSON.parse(res.data),
+                }
+              }
+            );
+            //
+          }).catch(function (err) {
+            console.log(err.xhr)
+            console.log(err.status)
+            console.log(err.message)
+          });
+      });
+    },
+  }
 ];
 
 export default routes;
